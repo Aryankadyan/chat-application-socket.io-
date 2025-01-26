7a8248d481dc23fbee2f9b3a7b8b0aaf813dfdf4
jai-max <kadyanaryan744@gmail.com>
2025-01-26 22:28:12 +0530

    Completed Chat Application

--- server/app.js
+++ server/app.js
@@ -1,44 +1,75 @@
-import express from 'express'; 
-import {Server} from "socket.io";
-import {createServer} from "http";
+import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "http";
 import cors from "cors";
+import jwt from "jsonwebtoken";
+import cookieParser from "cookie-parser";
 
-const port = 3000; 
+const secretKeyJWT = "asdasdsadasdasdasdsa";
+const port = 5000;
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server,{ 
-    cors: {
-        origin: "http://localhost:5173",
-        methods: ["GET", "POST"],
-        credentials: true,
-    } 
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
 });
-    app.use(cors({
-        origin:"http://localhost:5173",
-        methods:["GET","POST"],
-        credentials:true,
-    }),   
-)
 
- 
-app.get('/', (req, res) => {
-res.send('What is up?');  
-});  
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 
-io.on('connection',(socket)=>{
-    console.log("User Connected", socket.id);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-    socket.on("message", (data)=>{
-        console.log(data);
-        socket.broadcast.emit("receive-message", data);
-    })
+app.get("/login", (req, res) => {
+  const token = jwt.sign({ _id: "asdasjdhkasdasdas" }, secretKeyJWT);
 
-    socket.on("disconnect",()=>{
-        console.log("User Disconnected", socket.id);
-}) 
-})
+  res
+    .cookie("token", token, { httpOnly: true, secure: true, sameSite: "none" })
+    .json({
+      message: "Login Success",
+    });
+});
+
+io.use((socket, next) => {
+  cookieParser()(socket.request, socket.request.res, (err) => {
+    if (err) return next(err);
+
+    const token = socket.request.cookies.token;
+    if (!token) return next(new Error("Authentication Error"));
+
+    const decoded = jwt.verify(token, secretKeyJWT);
+    next();
+  });
+});
+
+io.on("connection", (socket) => {
+  console.log("User Connected", socket.id);
+
+  socket.on("message", ({ room, message }) => {
+    console.log({ room, message });
+    socket.to(room).emit("receive-message", message);
+  });
+
+  socket.on("join-room", (room) => {
+    socket.join(room);
+    console.log(`User joined room ${room}`);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
 
 server.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});         
+  console.log(`Server is running on port ${port}`);
+});   
